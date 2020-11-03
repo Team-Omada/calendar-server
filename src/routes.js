47cf1ae9e1793dacf7e2db5f5567a1f32a2a31d1
Cameron Collver <crcollver@gmail.com>
2020-11-02 18:07:45 -0800

    chore: added anticipated routes app functionality

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,21 @@ const UserController = require("./controllers/UserController");
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 
+// some of these look a bit complicated, but I think this follows REST standards
+router.get("/schedules"); // get all schedules
+router.post("/schedules"); // create a schedule
+router.get("/schedules/:scheduleID"); // get a specific schedule
+router.delete("/schedules/:scheduleID"); // delete a specific schedule
+router.put("/schedules/:scheduleID"); // update a specific schedule
+
+router.post("/schedules/:scheduleID/comments"); // add a comment to a schedule
+router.put("/schedules/:scheduleID/comments/:commentID"); // update an existing comment
+router.delete("/schedules/:scheduleID/comments/:commentID"); // delete a comment from a schedule
+
+router.get("/bookmarks/:userID"); // get bookmarks for a specific userID
+router.post("/bookmarks"); // add a new bookmark
+router.delete("/bookmarks/:userID/schedules/:scheduleID"); // delete a specific bookmark
+
 // sample of how we would use checkAuthenticated function
 // checkAuthenticated runs before the res.send() call
 // if auth fails, the res.send() will never be reached
